ca2dea067c55de30b13b9e88017eba171191fb1f
Rares Dan <goiararesdan@gmail.com>
2025-07-02 00:37:53 +0300

    frontend update

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -6,9 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // '/api': "https://deepfake-detection-backend.fly.dev",
+      '/api': "https://deepfake-detection-backend.fly.dev",
       // Change on Supabase deployment: https://deepfake-detection-kappa.vercel.app/dashboard
-      '/api': "http://localhost:5000", // Use this for local development
+      // '/api': "http://localhost:5000", // Use this for local development
     }
   },
 })
